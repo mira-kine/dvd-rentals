cd1de6ac1f3655ab2462a007b1d330d73e5b0e63
mira-kine <mirak0419@gmail.com>
2022-03-21 12:01:10 -0700

    finished exercise 1

--- exercise_1/exercise-1.test.js
+++ exercise_1/exercise-1.test.js
@@ -4,10 +4,10 @@ const pool = require('../lib/utils/pool');
 describe('exercise 1', () => {
   afterAll(() => pool.end());
 
-  it('find all address_id of addresses in the city of Dundee', async() => {
-    const { rows } = await pool.query(fs.readFileSync(`${__dirname}/exercise-1.sql`, 'utf-8'));
-    expect(rows).toEqual([
-      { address_id: 146 }
-    ]);
+  it('find all address_id of addresses in the city of Dundee', async () => {
+    const { rows } = await pool.query(
+      fs.readFileSync(`${__dirname}/exercise-1.sql`, 'utf-8')
+    );
+    expect(rows).toEqual([{ address_id: 146 }]);
   });
 });
